58784095761998d6e8dedf21ce7595c1284b4d66
Adriane Ablaza <aablaza04@localhost.localdomain>
2017-03-16 19:33:55 +0100

    Deberes17.03.2017

---
+++ Deberes17.03.2017/ejercicio-mayor.py
@@ -0,0 +1,12 @@
+# coding: utf8
+
+valor1 = input("Introduzca primer valor\n")
+valor2 = input("Introduzca segundo valor\n")
+
+if (valor1 > valor2):
+	print "valor 1 es más grande! "
+else :
+	if (valor1 == valor2):
+		print "Son iguales"
+	else:
+		print "valor 2 es más grande! "
---
+++ Deberes17.03.2017/ejercicio-swap.py
@@ -0,0 +1,11 @@
+# coding: utf8 
+
+mano_der = ("movil")
+mano_izq = ("bocadillo")
+mano_extra = ("movil")
+
+mano_extra = mano_der
+mano_der = mano_izq
+mano_izq = mano_extra
+
+print ("Ahora la mano_der es: "+str(mano_der)+" y la mano_izq es: "+str(mano_izq))
