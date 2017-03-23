38cc4ccdb4a5b6c9e52d3ba81cba368857777f44
ism7047186 <ism7047186@a21.informatica.escoladeltreball.org>
2017-03-23 08:33:03 +0100

    bucles23.03.2017

---
+++ bucles23.03.2017/ejercicio-bucle-contar2.py
@@ -0,0 +1,13 @@
+# coding: utf8
+
+contador = 1
+limite = input ("Hasta que numero desea el amo?")
+salir = False
+while (salir == False):
+	print contador
+	
+	if (contador == limite) :
+		salir = True
+	contador = contador+1
+
+
