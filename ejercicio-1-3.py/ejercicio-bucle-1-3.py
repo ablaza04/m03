49a5e61bee027734380d029327fbeddac17b496b
Adriane Ablaza <aablaza04@localhost.localdomain>
2017-05-03 19:53:16 +0200

    ejercicio-1-3.py

---
+++ ejercicio-1-3.py/ejercicio-bucle-1-3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+cont = 1
+salir = False
+while (salir == False):
+	print cont
+	
+	
+	if (cont == 3):
+		salir = True
+	
+	cont = cont+1	
+	
+		
---
+++ ejercicio-1-3.py/ejercicio-for-1-3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+for cont in xrange (1,4):
+	print cont
+	
+	if (cont == 3):
+		salir = True
+	
+	cont = cont+1
