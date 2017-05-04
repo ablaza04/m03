0ccf10c109e97927db251a1d8dc31090673540f2
Adriane Ablaza <aablaza04@localhost.localdomain>
2017-05-04 23:50:14 +0200

    Deberes04-05-2017.py

---
+++ Deberes04-05-2017.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+for fil in xrange (1,5,1):
+	
+	for col in xrange (1,4,1):
+		
+		if (fil == 1):
+			if (col == 2):
+				print "A"
+			if (col == 3):
+				print "B"
+			if (col == 4):
+				print "C"
+			
+			else:
+				print "-"
+		if (fil == 3):
+			if (col ==3):
+				print "*"
+		if (fil == 4):
+			if (col == 2):
+				print "*"
+			
+			
+				
