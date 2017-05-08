b0a533bf28ddff98067d099437fbde8cebab6c1c
Adriane Ablaza <aablaza04@localhost.localdomain>
2017-05-09 00:15:24 +0200

    for-examen.py

---
+++ for-examen.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# myrange
+def myrange (inici, fi, augment):
+
+    while inici <= fi:
+		
+        yield inici	
+        
+	inici = inici + augment
+
+#fil = fila i col = columna
+for fil in myrange (1,5,1):
+	
+	for col in myrange (1,4,1):
+		
+		if (fil == 1):
+			if (col == 2):
+				print "A",
+			if (col == 3):
+				print "B",
+			if (col == 4):
+				print "C",
+			else:
+				print "-",
+		
+		else:
+			
+				
+			if (col == 1):
+				print fil-1,
+			
+			elif (
+					((fil == 3) and (col == 3))
+					or ((fil == 4) and (col == 2))
+				 ): 			 
+				print "*",
+					  
+			else:
+				print "-",
+
+	print ""
