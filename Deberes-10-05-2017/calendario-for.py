893a6aa5ec5edd9856128a31188229008d933fdf
Adriane Ablaza <aablaza04@localhost.localdomain>
2017-05-10 23:56:29 +0200

    Deberes-10-05-2017

---
+++ Deberes-10-05-2017/calendario-for.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# myrange
+def myrange (inici, fi, augment):
+
+    while inici <= fi:
+
+        yield inici
+
+	inici = inici + augment
+
+for fil in myrange (1,6,1):
+	
+	for col in myrange (1,7,1):
+		
+		if (fil == 1):
+			if (col == 1):
+				print "Lun",
+			elif (col == 2):
+				print "Mar",
+			elif (col == 3):
+				print "Mie",
+			elif (col == 4):
+				print "Jue",
+			elif (col == 5):
+				print "Vie",
+			elif (col == 6):
+				print "Sab",
+			elif (col == 7):
+				print "Dom",
+				
