d49227cfbbf532303e080f80bc86dcd6c8ed4092
ism7047186 <ism7047186@a21.informatica.escoladeltreball.org>
2017-05-11 08:53:23 +0200

    Deberes-10-05-2017

--- Deberes-10-05-2017/ajedrez-for.py
+++ Deberes-10-05-2017/ajedrez-for.py
@@ -14,7 +14,8 @@ for fil in myrange (1,8,1):
 	
 	for col in myrange (1,8,1):
 		
-		if (fil %2 == 0) or (col %2 == 0):
+		if ((fil %2 == 0 and col %2 == 0) or
+			(fil %2 == 1 and col %2 == 1)):
 			print "x",
 		else:
 			print " ",
